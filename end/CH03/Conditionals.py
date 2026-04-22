96757e6de663da503299fead3f904bb93418d08d
SafiatouTarore <Traore.Safiatou@student.greenrivere.edu>
2026-04-22 00:13:41 +0000

    Added a few elif and else statements

--- end/CH03/Conditionals.py
+++ end/CH03/Conditionals.py
@@ -3,9 +3,13 @@ Create a  conditinal that takes a input and if its a yes then return a reply
 '''
 
 #This is ging to take an input from the user
-answer = input("Is today a good day? (y/n) ")
+answer = input("Is today a good day? (y/n) ").lower ()
 
 
 #Its an if statement checking if the string is equal to y and if so print yes it is
 if answer == "y":
     print("Yes it is")
+elif answer == "n":
+    print("Im sorry")
+else:
+    print("Please try again")
