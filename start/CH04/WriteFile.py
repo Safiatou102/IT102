18e269e03cad3180586c9c0a870a4bf49009b67a
SafiatouTarore <Traore.Safiatou@student.greenrivere.edu>
2026-04-30 22:19:44 +0000

    Created a write script file

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
-# By 
+# By Safiatou t - instructings
+# Script 1 - collect info and save to hackme.txt
+
+'''
+Write ascript that saves user input into a file, that data about the user
+'''
+
+#These variables are questions that need to be answered
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
+pet = input("What is your first pets pet? ")
+maiden = input("What is your mother maiden name? ")
+school = input("What elementary school did you intend? ")
+
+with open("hackme.txt", "w") as file:
+     file.write(f"Name: {name}\n")
+     file.write(f"Favorite Color: {color}\n")
+     file.write(f"First Pet: {pet}\n")
+     file.write(f"Mothers maiden Name: {maiden}\n")
+     file.write(f"Elementary School: {school}\n")
+
+print("Saved to hakme.txt great work!")
