fe5722c26c5d6cdbdbd7f4ce136d5d0fde0c5e99
SafiatouTarore <Traore.Safiatou@student.greenrivere.edu>
2026-04-30 23:22:21 +0000

    created a function script

---
+++ start/CH04/Function.py
@@ -0,0 +1,26 @@
+'''
+Create a conditional that takes a input and if its a yes then return a reply
+'''
+
+#This is going to take an input from the user
+answer =input("Is today a good day? (y/n)").lower()
+
+"""
+Loops yea it is if the response is = to yes
+"""
+def send_message():
+    for x in range(10):
+        print("Yeah it is!")
+
+#Its an if statement chacking if the string is equal to and if so print yes it is 
+if answer == "y":
+   send_message()
+elif answer == "n":
+    print("Im sorry")
+else:
+    print("Please try again ")
+
+
+
+
+
