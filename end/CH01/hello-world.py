7ac2e4572053ef1e2c136fa3316bbe68ed944d35
SafiatouTarore <Traore.Safiatou@student.greenrivere.edu>
2026-05-18 23:11:49 +0000

    I created a hello world script.

---
+++ end/CH01/hello-world.py
@@ -0,0 +1,15 @@
+"""
+This script prompts the user for their name
+and prints a greeting with a positive affirmation.
+"""
+
+# Prompt the user for their name
+guest_name = input("What is your name? ")
+
+# Say hello
+print("Hello,", guest_name)
+
+# Positive affirmation
+print("Today is going to be a great day!")
+
+#Prompt user for age, and then add 2 years -- lecture
